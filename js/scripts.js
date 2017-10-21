4ab9a5f176e1e669970edc596060a8a0c9d3c24f
Nathan Moseley <Nate@Nathans-MacBook-Air.local>
2017-10-20 17:30:11 -0700

    Finish JS Data Pull

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,71 @@
-(function($){
-
-  
-  $('#menu').on('change',function(){
-   var choice = $('#menu option:selected' ).val();
-   $("ul").empty();
-
-var url = 'https://api.nytimes.com/svc/topstories/v2/' + choice +'.json';
-url += '?' + $.param({
-  'api-key': '4802e2aa376c4a9f96db22b0dc2c01cf'
-});
-$.ajax({
-  url: url,
-  method: 'GET',
-})
-.done(function(data) {
-  $('.headlines').empty();
-  for(var i = 0; i <= 12; i++) {
-  var title = data.results[i].abstract
-  var image = data.results[i].multimedia[4].url
-  var url = data.results[i].url
-  console.log(title);
-  $('.headlines').append('<a style="background-image: url(' + image + ')" href="' + url + '"' + '>'  + '<div class="list-image">' + title + '</div></a>');
-  }
-  
-  console.log(data);  
-})
+(function ($) {
 
 
-.fail(function(err) {
-  throw err;
-});
+  $('#menu').on('change', function () {
+    var choice = $('#menu option:selected').val();
+    $("ul").empty();
 
-
-})
+    var url = 'https://api.nytimes.com/svc/topstories/v2/' + choice + '.json?api-key=4802e2aa376c4a9f96db22b0dc2c01cf'
 
 
 
+    $.ajax({
+      url: url,
+      method: 'GET',
+    })
+
+      .done(function (data) {
+        $('.headlines').empty();
+        // for (var i = 0; i <= 12; i++) {
+        //   var title = data.results[i].abstract,
+        //     image = data.results[i].multimedia[4].url,
+        //     url = data.results[i].url
+        // }
+        // console.log(title);
+        // $('.headlines').append('<a style="background-image: url(' + image + ')" href="' + url + '"' + '>' + '<div class="list-image">' + title + '</div></a>');
+        // }
+
+        
+
+        function test() {
+          return data.results.filter(function (item) {
+            return item.multimedia.length !== 0
+            
+          }).splice(0, 12)
+        }
+        console.log(data)
+
+        $.each(test(), function (index, value) {
+           var imglink = '';
+           imglink = value.multimedia[4].url;
+          
+          var z = '';
+
+              z = '<a style="background-image: url(' + imglink + ')" href="' + url + '"' + '>' + '<div class="list-image">' + value.title + '</div></a>'
+
+              // z = '<li class="article"><a style="background-image: url(' + imglink + ')" + 'href="' + value.multimedia.url + " + '</a><div><p>' + value.title + '</div></p></li>';
+
+          
+
+
+  $('.headlines').append(z)
+            })
+
+        console.log("text", test());
+
+        // $.each(data.results.filter(function(item) { return item.multimedia.length !== 0 }).splice(0, 12), function(index, value) {
+        //    console.log('data.results:', value)
+
+
+
+
+
+        // .fail(function (err) {
+        //   throw err;
+        // });
+
+
+      })
+  })
 
 })(jQuery);
