cbc15011f895a9f8684ed1776be30855fe52cd8b
Nathan Moseley <Nate@Nathans-MacBook-Air.local>
2017-10-22 13:37:32 -0700

    Finish CSS

--- README.md
+++ README.md
@@ -1,19 +1,26 @@
-# NYT 
+# NYT Instanews
 One-page, responsive website that allows a user to filter top news story categories via the New York Times API.
 
 ## Setup
 This page is optimized to run on ?????????
 ## References
 
+This page was built using technology including:
+
+CSS
+SASS
+jQuery
+
+
 ```
-npm start
+
 ```
 
 ## Run Script
 
 ```
 NYT_API_KEY=<api-key>
-npm run local
+
 ```
 
 ## Tests
--- js/scripts.js
+++ js/scripts.js
@@ -19,16 +19,8 @@
           "align-items": "center",
           flex: "1 0 auto",
           height: "auto"
-          // 'max-width': "400px"
         });
 
-        // $("#header").css({
-        //   "align-items": "center",
-        //   flex: "1 0 auto",
-        //   height: "auto",
-        //   'max-width': "600px",
-        //   display: "flex"
-        // });
 
       var url = 'https://api.nytimes.com/svc/topstories/v2/' + choice + '.json?api-key=4802e2aa376c4a9f96db22b0dc2c01cf'
 
@@ -40,16 +32,6 @@
 
         .done(function (data) {
           $('.headlines').empty();
-          // for (var i = 0; i <= 12; i++) {
-          //   var title = data.results[i].abstract,
-          //     image = data.results[i].multimedia[4].url,
-          //     url = data.results[i].url
-          // }
-          // console.log(title);
-          // $('.headlines').append('<a style="background-image: url(' + image + ')" href="' + url + '"' + '>' + '<div class="list-image">' + title + '</div></a>');
-          // }
-
-
 
           function test() {
             return data.results.filter(function (item) {
@@ -69,27 +51,10 @@
 
             z = '<a style="background-image: url(' + imglink + ')" href="' + c + '"' + '>' + '<div class="titlebg">' + '<div class="list-title">' + value.abstract + '</div>' + '</div>' + '</a>' 
 
-            // z = '<li class="article"><a style="background-image: url(' + imglink + ')" + 'href="' + value.multimedia.url + " + '</a><div><p>' + value.title + '</div></p></li>';
-
-
-
-
             $('.headlines').append(z)
           })
 
           console.log("text", test());
-
-          // $.each(data.results.filter(function(item) { return item.multimedia.length !== 0 }).splice(0, 12), function(index, value) {
-          //    console.log('data.results:', value)
-
-
-
-
-
-          // .fail(function (err) {
-          //   throw err;
-          // });
-
 
         })
     })
